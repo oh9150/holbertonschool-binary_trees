52ca09e513a2c86cdc11702e84688c18865d4d81
root <6351@holbertonstudents.com>
2023-05-14 13:26:27 -0700

    fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left - inserts a node at the left of @parent, if it has
+ * a left node, it becomes the new node's left child.
+ * @parent: the parent of the node
+ * @value: the value of the node
+ *
+ * Return: the new node, NULL if it failed
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
