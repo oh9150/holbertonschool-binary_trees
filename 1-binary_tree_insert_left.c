b426037dc8fed8ae1092c3fc4c10bfd61f1eed11
root <6351@holbertonstudents.com>
2023-05-14 21:27:07 -0700

    in-order traversal + insert right + pre-order traversal betty fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,6 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		free(new_node);
 		return (NULL);
 	}
+	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = parent->left;
 	parent->left = new_node;
---
+++ 1-binary_tree_insert_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a leaf at the right of a parent node.
+ * If there's already a node, it becomes the new node's right child.
+ * @parent: the parent node
+ * @value: the value of the new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->right = parent->right;
+	parent->right = new_node;
+
+	return (new_node);
+}
