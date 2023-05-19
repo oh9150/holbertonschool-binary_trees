e3ec1ca64ba7b31d1872d5dce0b40466cf1fc910
root <6351@holbertonstudents.com>
2023-05-19 16:33:32 -0700

    made it in one function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,30 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-
-	return (binary_tree_height_recursive(tree));
-}
-
-/**
- * binary_tree_height_recursive - recursive function to find the height
- * @tree: the tree
- * @height: the current height
- * @max_height: the max height
- * Return: the height
- */
-size_t binary_tree_height_recursive(const binary_tree_t *tree)
-{
 	size_t left, right;
+
 	if (!tree)
 		return (0);
-
-	left = binary_tree_height_recursive(tree->left);
-	right = binary_tree_height_recursive(tree->right);
-
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 	if (left > right)
 		return (left + 1);
-
 	return (right + 1);
 }
