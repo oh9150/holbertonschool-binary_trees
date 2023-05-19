a8f19310436ed7932b95826fa777c947b75f152e
root <6351@holbertonstudents.com>
2023-05-19 16:57:45 -0700

    binary_tree_nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - finds the nodes with at least one child under a given
+ * tree
+ * @tree: the tree
+ * Return: the ammount of nodes
+ */
+size_t binary_tree_nodes(binary_tree_t *tree)
+{
+	size_t right, left;
+
+	if (!tree)
+		return (0);
+	if (!(tree->right || tree->left))
+		return (0);
+
+	left = binary_tree_nodes(tree->left);
+	right = binary_tree_nodes(tree-right);
+
+	return (left + right);
+}
