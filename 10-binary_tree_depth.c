094a8c5233b90ecbd7e50acdb2766053e99be25a
root <6351@holbertonstudents.com>
2023-05-19 16:44:14 -0700

    fix

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,6 +8,8 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 	if (!tree->parent)
 		return (0);
 
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,11 +11,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (!tree->parent)
-		return (0);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
+
+	if (!tree->parent)
+		return (0);
 
 	if (left > right)
 		return (left + 1);
