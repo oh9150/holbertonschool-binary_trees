71275b9e1022c202a0a87c32c69097f3b44c6b84
root <6351@holbertonstudents.com>
2023-05-19 16:41:22 -0700

    height fix + depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,4 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - finds
+ * binary_tree_depth - finds the depth of a given node
+ * @tree: the node
+ *
+ * Return: the depth of the node, or 0 tree == NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree->parent)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,9 +11,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	if (!tree->parent)
+		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
+
 	if (left > right)
 		return (left + 1);
+
 	return (right + 1);
 }
