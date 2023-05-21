1dbb811bd5f108d13bae716e8fd42e75e92937d8
root <6351@holbertonstudents.com>
2023-05-20 23:22:29 -0700

    fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,7 +8,7 @@
  * Return: the lowest common ancestor or NULL if it wasn't found or if either
  * of the nodes == NULL
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 	int depth1, depth2;
 
