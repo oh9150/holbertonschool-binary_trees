69aef544ff5466b49851eda1570e29d59585a00a
root <6351@holbertonstudents.com>
2023-05-20 23:16:17 -0700

    binary trees lowest common ancestor (hopefully it works)

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of 2 nodes in a
+ * binary tree
+ * @first: the first node
+ * @second: the second node
+ * Return: the lowest common ancestor or NULL if it wasn't found or if either
+ * of the nodes == NULL
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+{
+	int depth1, depth2;
+
+	if (!first || !second)
+		return (NULL);
+
+	depth1 = binary_tree_depth (first);
+	depth2 = binary_tree_depth (second);
+
+	if (depth1 > depth2)
+		binary_trees_ancestor(first, second->parent);
+	if (depth1 < depth2)
+		binary_trees_ancestor(first->parent, second);
+	else
+	{
+		if (first->parent == second->parent)
+			return (first->parent);
+
+		binary_trees_ancestor(first->parent, second->parent);
+	}
+}
+
+/**
+ * binary_tree_depth - finds the depth of a given node
+ * @tree: the node
+ *
+ * Return: the depth of the node, or 0 tree == NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->parent)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_sibling - finds the sibling of a node in a binary tree
- * @tree: the node
+ * @node: the node
  * Return: a pointer to the sibling or NULL if the node is NULL or there's no
  * sibling
  */
@@ -15,9 +15,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent->left == node)
-		return (node->right);
+		return (node->parent->right);
 	if (node->parent->right == node)
-		return (node->left);
+		return (node->parent->left);
 
 	return (NULL);
 }
