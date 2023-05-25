dd8ca7d6398bb8624b5133f1bab7cd1294ff3f1f
root <6351@holbertonstudents.com>
2023-05-24 20:39:22 -0700

    binary trees ancestor fix=

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,4 +2,9 @@
 
 /**
  * binary_tree_levelorder - goes through a binary using level order traversal
- * @
+ * @tree: the tree
+ * @func: the function to execute on each node's value
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+
