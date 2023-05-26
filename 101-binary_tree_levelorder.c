ddf6d88d1a32db8753c084abada9cfc61edb646a
root <6351@holbertonstudents.com>
2023-05-25 17:46:29 -0700

    rotate left

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -9,6 +9,9 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	size_t h = binary_tree_height(tree), i;
 
+	if (!tree || !func)
+		return;
+
 	for (i = 0; i <= h; i++)
 		print_level(tree, i, func);
 }
---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - performs a left-rotation on a binary tree
+ * @tree: the tree
+ * Return: a pointer to the new root node
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *parent;
+
+	if (!tree)
+		return(NULL);
+
+	if (tree->parent)
+		return (NULL);
+
+	tree->parent = tree->right;
+
+	/* setting the bottom node*/
+	if (tree->right)
+	{
+		tree->parent->parent = NULL;
+		tree->right = tree->right->left;
+		if (tree->right->left)
+			tree->right->left->parent = tree;
+	}
+
+	return (tree->parent);
+}
